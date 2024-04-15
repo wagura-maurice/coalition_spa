8d1ad98db1a07164ecaefec5eab580b72f445250
Wagura Maurice <business@waguramaurice.com>
2024-04-15 06:30:45 +0300

    feat: create postcss.config.js and tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  content: [],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
