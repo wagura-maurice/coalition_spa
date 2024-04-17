bbcc9e06b22582550d2d3fed40dd7537b5692dbd
Wagura Maurice <business@waguramaurice.com>
2024-04-17 05:10:08 +0300

    update helper.ts

--- src/utility/helper.ts
+++ src/utility/helper.ts
@@ -1,4 +1,6 @@
 // src/utility/helper.ts
+import { format } from 'date-fns';
+
 export function getQueryString(filters: any): string {
   let queryString = '';
   for (const key in filters) {
@@ -14,6 +16,17 @@ export function getQueryString(filters: any): string {
   return queryString;
 }
 
+export function formatDueDate(dueDate: any): string {
+  // Check if dueDate is already a Date object
+  if (!(dueDate instanceof Date)) {
+    // Parse dueDate to a Date object if it's not already
+    dueDate = new Date(dueDate);
+  }
+  
+  if (!dueDate || isNaN(dueDate.getTime())) return ''; // Return empty string if dueDate is null, undefined, or not a valid date
+  return format(dueDate, 'MMM dd, yyyy hh:mm:ss a'); // Customize the format as needed
+}
+
 export function toUpperCase(value: string): string {
   return value.toUpperCase();
 }
@@ -25,3 +38,52 @@ export function toLowerCase(value: string): string {
 export function toTitleCase(value: string): string {
   return value.replace(/\b\w/g, char => char.toUpperCase());
 }
+
+export const Priority = {
+  URGENT: 'Urgent',
+  HIGH: 'High',
+  MEDIUM: 'Medium',
+  LOW: 'Low',
+  CRITICAL: 'Critical',
+  NORMAL: 'Normal',
+  EMERGENCY: 'Emergency',
+  DEFERRED: 'Deferred',
+  OPTIONAL: 'Optional',
+  ROUTINE: 'Routine'
+};
+
+export function getPriorityName(priority: number): string {
+  switch (priority) {
+    case 0: return Priority.URGENT;
+    case 1: return Priority.HIGH;
+    case 2: return Priority.MEDIUM;
+    case 3: return Priority.LOW;
+    case 4: return Priority.CRITICAL;
+    case 5: return Priority.NORMAL;
+    case 6: return Priority.EMERGENCY;
+    case 7: return Priority.DEFERRED;
+    case 8: return Priority.OPTIONAL;
+    case 9: return Priority.ROUTINE;
+    default: return '';
+  }
+}
+
+export const Status = {
+  PENDING: 'Pending',
+  PROCESSING: 'Processing',
+  PROCESSED: 'Processed',
+  COMPLETED: 'Completed',
+  CANCELLED: 'Cancelled'
+};
+
+export function getStatusName(status: number): string {
+  switch (status) {
+    case 0: return Status.PENDING;
+    case 1: return Status.PROCESSING;
+    case 2: return Status.PROCESSED;
+    case 3: return Status.COMPLETED;
+    case 4: return Status.CANCELLED;
+    default: return '';
+  }
+}
+
